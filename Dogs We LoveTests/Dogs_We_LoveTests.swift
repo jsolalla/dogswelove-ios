2df65fc5085908e4ba5f68b534869b5ae2893b85
Jesús Santa Olalla <jesus.santaolalla.picazo@gmail.com>
2020-01-27 19:56:06 -0600

    * Added Xcode initial project

---
+++ Dogs We LoveTests/Dogs_We_LoveTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dogs_We_LoveTests.swift
+//  Dogs We LoveTests
+//
+//  Created by Jesus Eduardo Santa Olalla Picazo  (Vendor) on 1/27/20.
+//  Copyright © 2020 Codikas. All rights reserved.
+//
+
+import XCTest
+@testable import Dogs_We_Love
+
+class Dogs_We_LoveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
